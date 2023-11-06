d23482a475e8d70d144ff7ccc09f4a6a91fe962a
CecylBot <115835582+CecylBot@users.noreply.github.com>
2023-11-06 17:44:02 +0100

    Delete youmad directory

--- youmad/main/libs/cart_number.py
+++
@@ -1,7 +0,0 @@
-def calculate(request):
-    if 'cart' in request.session:
-            cart = request.session['cart']
-            num_products_in_cart = sum(cart.values()) 
-    else:
-            num_products_in_cart = 0 
-    return num_products_in_cart
